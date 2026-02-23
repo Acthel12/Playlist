800e8ecf185ab0b9975847cc821c8afcedf81011
Gabriel Carneiro <gecgpro12@gmail.com>
2026-02-23 19:27:47 -0400

    Creado La clase Nodo que sirve para crear nodos doblementenlazados

---
+++ Playlist/src/main/java/com/mycompany/playlist/Nodo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Nodo {
+    String cancion;
+    Nodo siguiente;
+    Nodo anterior;
+    
+    public Nodo (String cancion){
+        this.cancion = cancion;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
