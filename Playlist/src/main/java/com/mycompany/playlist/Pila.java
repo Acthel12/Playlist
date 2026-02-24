91aaddb884d3a31baee4652d4e7e7e5bbae02ced
Gabriel Carneiro <gecgpro12@gmail.com>
2026-02-24 10:38:41 -0400

    Add: Agregado la pila

---
+++ Playlist/src/main/java/com/mycompany/playlist/Pila.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Pila {
+        Nodo cabeza;
+        
+        public Pila (){
+            this.cabeza = null;
+        }
+        
+        public void Apilar(Nodo cancion){
+            if (this.cabeza == null){
+                this.cabeza = cancion;
+            }
+            else {
+                cancion.siguiente = this.cabeza;
+                this.cabeza = cancion;
+            }
+        }
+        public Nodo Desapilar(){
+            if (this.cabeza == null){
+                System.out.println("Error, El historial esta vacio");
+                return null;
+            }
+            else {
+                Nodo actual = this.cabeza;
+                this.cabeza = this.cabeza.siguiente;
+                actual.siguiente = null;
+                
+                return actual;
+            }
+        }
+        public void MostrarHistorial(){
+            if (this.cabeza != null){
+                System.out.println("Historial de canciones");
+                int contador = 0;
+                Nodo siguiente = this.cabeza;
+                while ( siguiente != null){
+                    contador += 1;
+                    System.out.println(contador + ". " + siguiente.cancion);
+                    siguiente = siguiente.siguiente;
+                }
+            }
+            else {
+                System.out.println("El historial esta vacio");
+            }
+        }
+}
